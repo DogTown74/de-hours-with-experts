9913243f2c2518c07828e4db60387988e6ca5e94
Nathan <nathan.liston@gmail.com>
2020-01-21 11:21:27 -0600

    String sort to reorder

--- python/next_biggest_number.py
+++ python/next_biggest_number.py
@@ -1,43 +1,26 @@
+#Nathan Liston 1-23-2020
 #!/usr/bin/python3
 import sys
 
 def main():
-    parm = sys.argv[1]
-    nbr = int(sys.argv[1])
-    nbrlen = len(sys.argv[1])
-    biggestnbr = 0
-    descending = "".join(sorted(str(parm), reverse=True))
-    print("desc", descending)
-    #for loop to loop through the nbr
-    for x in reversed(range(nbrlen)):
-            print("nbrlen",x)
-            print(parm[:(x+1)])
-            print("next biggest nbr", next_biggest_number(parm[:(x+1)]))
+    #get the nbr passed
+    num = sys.argv[1]
+    print("Input: ", num)
+    #call function to get biggest nbr
+    newnum = next_biggest_number(num)
+    if newnum > int(num):
+        print("Output: ", newnum)
     else:
-            print("finished Loop 1")
-    #  print(next_biggest_number())
-    print("im in main")
-    print(sys.argv[1])
-    #next_biggest_number(1)
-    return 0
+        print("Output: ", -1)
 
 
 def next_biggest_number(num):
-    print("im in next biggest number", num)
-    bignbr = 0
-    for c in str(num):
-        i = int(c)
-        if i > bignbr:
-            bignbr = i
-            print("bignbr is now", bignbr)
-    #else:
-    #    print("finished loop2")
-    print("returning", bignbr)
-    return bignbr
+    #reorder the nbr
+    descending = int("".join(sorted(str(num), reverse=True)))
+    return descending
 
 if __name__ == "__main__":
     main()
-
 
 
 
